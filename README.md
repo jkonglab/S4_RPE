4695819494b5e29d501c6465c2ffa6e0d159f96e
jkonglab <jkong@gsu.edu>
2022-11-04 15:03:30 -0400

    Add files via upload
    add readme.md; update config.py

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# S4_RPE
-Self-Supervised Semantic Segmentation for RPE cells
+# S<sup>4</sup>
+We provide our PyTorch implementation of our S<sup>4</sup> model. The developed method trains a deep neural network with unlabeled images. Specifically, a reconstruction loss by the encoder-decoder component and a pairwise representation loss by auxiliary MLP heads are used to extract structural information of cell borders. Additionally, a morphological loss guides the network to output binary segmentation results.
+
+The model presents better performance compared with some SOTA approaches such as UNet, DeepLab, MultiResUNet, and[CellPose](https://www.cellpose.org/) in the RPE cell border segmentation task.
+***
+### Prerequisites
+* CPU or NVIDIA GPU
+* Linux or macOS
+* Python 3.10
+* PyTorch 1.8.1
+***
+### Usage
+* Train model:
+```
+python train.py
+```
+* Test model:
+```
+python test.py
+```
+### Note
+* `model.py` constructs the networks including the encoder, decoder, and MLPs in our developed model.
+* `utils.py` defines modules to build the network, loss functions, etc.
+* In `config.py` users can change configurations including I/O paths, filter size and number for the first layer of the network, and traning hyperparameters. More explanations are given in the file.
--- config.py
+++ config.py
@@ -3,29 +3,27 @@
 
 class Config(object):
     # path
-    src_path = "data"
-    dst_path = "results"
-    val_path = "validation"
-    log_path = "logs/"
-    model_path = "checkpoints"
+    src_path = "data"           # path to load training data
+    log_path = "logs/"          # path to save training logs
+    model_path = "checkpoints"  # path to save (intermediate) models
 
     # training parameters
-    gpu = False
-    img_sz = 64
-    batch_sz = 64
-    n_f = 64
-    n_feat_dim = 2048
-    n_hidden_dim = 512
-    lr = 1e-4
-    beta1 = 0.5
-    beta2 = 0.999
-    wd = 1e-4
-    max_epoch = 150
-    start_mix = 30
-    end_mix = 70
-    lambda_topo = 2
-    lambda_metric = 0.5
-    save_freq = 10
+    gpu = False         # whether to train with GPU
+    img_sz = 64         # image size
+    batch_sz = 64       # batch size
+    n_f = 64            # number of filters at the highest level
+    n_feat_dim = 2048   # length of representation vector
+    n_hidden_dim = 512  # number of nodes in MLP hidden layers
+    lr = 1e-4           # learning rate
+    beta1 = 0.5         # parameter 1 for Adam optimizer
+    beta2 = 0.999       # parameter 2 for Adam optimizer
+    wd = 1e-4           # weight decay rate
+    max_epoch = 150     # max epoch number
+    start_mix = 30      # start time of training strategy transition
+    end_mix = 70        # end time of training strategy transition
+    lambda_topo = 2     # weight factor for topology loss
+    lambda_metric = 0.5 # weight factor for pairwise learning loss
+    save_freq = 25      # model saving frequency
 
 
 opt = Config()
